486e28039bf49f6a269f6f7ce44fe2723c109520
Corbalg <corbalg@gmail.com>
2023-11-24 16:18:51 -0300

    subir proyecto a rama main

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const publicPath = path.resolve(__dirname, '../public')
+app.use(express.static(publicPath))
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+})
+                  
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
+})
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+})
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
