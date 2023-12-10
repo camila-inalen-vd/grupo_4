d528f855a2bf195753e15ad11ec4d6de098f265a
camilavd <camilavd045@gmail.com>
2023-12-10 20:34:14 -0300

    Eliminado guiaList

--- src/routes/guiaList.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path')
-const productListController = require('../controllers/guiaList.js')
-
-router.get('/list', productListController.productList)
-
-module.exports = router;
