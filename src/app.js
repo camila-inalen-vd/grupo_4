8ba49a6977f220310262d08f01b2c2b515f4b561
fedejael <federicojael.canto@gmail.com>
2023-12-10 20:12:35 -0300

    Arreglando cambios

--- src/app.js
+++ src/app.js
@@ -6,14 +6,9 @@ const publicPath = path.resolve(__dirname, '../public')
 
 //Requerir rutas
 const mainRoutes = require('./routes/mainRoutes.js');
-<<<<<<< HEAD
-const productRoutes = require('./routes/productRoutes.js');
-const productListRoutes = require('./routes/productList.js')
-=======
 const productRoutes = require('./routes/productsRoutes.js');
 const productlistRoutes = require('./routes/guiaList.js')
 const userRoutes = require('./routes/userRoutes.js');
->>>>>>> eba07e37a0d8ecb291b669d473310bb85341a51e
 
 //Carpeta views y public.
 app.set('views', path.resolve(__dirname, 'views'));
@@ -24,18 +19,11 @@ app.set('view engine', 'ejs')
 
 //Rutas
 app.use('/', mainRoutes);
-<<<<<<< HEAD
-app.use('/detalle', productRoutes)
-//Esta es la ruta de fede
-app.use('/product', productListRoutes)
-
-=======
 //ProductDetail y ProductList
 app.use('/product', productRoutes)
 app.use('/product', productlistRoutes)
 //Rutas login y register
 app.use('/user', userRoutes)
->>>>>>> eba07e37a0d8ecb291b669d473310bb85341a51e
 
 //Estas rutas hay que modificarlas con lo aprendido en MVC. Les puse nombre para que los que estan a cargo de cada una las identifiquen.
 
@@ -44,7 +32,6 @@ app.get('/productCart', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/productCart.html'))
 })
 
-<<<<<<< HEAD
 //Gonza
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
@@ -59,15 +46,6 @@ app.post('/register', (req, res) => {
 app.post('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'))
 })
-
-
-=======
-//Fede
-app.get('/productlist', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productList.html'))
-})
->>>>>>> eba07e37a0d8ecb291b669d473310bb85341a51e
-
 
 //Arranque del servidor (Lo tiré abajo de todo para evitar errores al leerse antes que otras ejecuciones)
 const port = process.env.PORT || 3000;
