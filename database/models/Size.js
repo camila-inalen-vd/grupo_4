7aa4579669e077aadec07905c3860a57ec049e9b
fedejael <federicojael.canto@gmail.com>
2024-02-20 15:46:12 -0300

    Creando el modelo Size

---
+++ database/models/Size.js
@@ -0,0 +1,19 @@
+const alias = "Size";
+const cols = {
+    id: {
+        type: DataTypes.INTEGER, 
+        primaryKey: true,
+        autoIncrement: true
+    },
+    number: {
+        type: DataTypes.INTEGER, 
+        allowNull: false
+    }
+};
+const config = {
+    tableName: "sizes",
+    timestamps: false
+};
+const Size = sequelize.define(alias, cols, config);
+
+return Size;
