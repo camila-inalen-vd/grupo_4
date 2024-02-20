8f92c8fed4a9868ad126409a96e150c078ca8304
fedejael <federicojael.canto@gmail.com>
2024-02-20 14:41:37 -0300

    Agregando sequelize

---
+++ database/models/Products.js
@@ -0,0 +1,59 @@
+const alias = "Product";
+const cols = {
+    id: {
+        type: DataTypes.INTEGER, 
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING, 
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.INTEGER, 
+        allowNull: false
+    },
+    dues: {
+        type: DataTypes.INTEGER, 
+        allowNull: false
+    },
+    discount: {
+        type: DataTypes.INTEGER, 
+        allowNull: false
+    },
+    stock: {
+        type: DataTypes.INTEGER, 
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING, 
+        allowNull: false
+    },
+    large_description: {
+        type: DataTypes.TEXT, 
+        allowNull: false
+    },
+    upper: {
+        type: DataTypes.STRING 
+    },
+    cover: {
+        type: DataTypes.STRING
+    },
+    sole: {
+        type: DataTypes.STRING
+    },
+    origin: {
+        type: DataTypes.STRING
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+};
+const config = {
+    tableName: "products",
+    timestamps: false
+};
+const Product = sequelize.define(alias, cols, config);
+
+return Product;
