dfa0e14d329bb79b2a4f551b098dcb38978e287e
NoeCar <noeliacarabajal0@gmail.com>
2024-02-20 15:09:54 -0300

    sequelize user

---
+++ database/models/User.js
@@ -0,0 +1,48 @@
+const alias = "User";
+const cols = {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true 
+    },
+
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    
+    admin: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+    
+};
+ const config = {
+    tableName: "users",
+    timestamps: false
+};
+
+const User = sequelize.define(alias, cols, config);
+
+return User;
+
