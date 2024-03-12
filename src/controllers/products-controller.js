c21f0d43e62ac4fbeb05186f495fe8b43cfec879
camilavd <camilavd045@gmail.com>
2024-03-11 23:47:04 -0300

    Ahora sí, añadido el then al controlador de creación de producto y manejo de errores.

--- src/controllers/products-controller.js
+++ src/controllers/products-controller.js
@@ -45,21 +45,14 @@ const productsController = {
         try {
             let brand = await db.Brand.findAll()
             let color = await db.Color.findAll()
-            res.render('products/create', { brand, color })
+            let size = await db.Size.findAll()
+            res.render('products/create', { brand, color, size })
         }
         catch (error) {
             res.send(error)
         }
     },
-
-    //Renderizacion de la vista de editar producto. Aca vamos a usar un findByPk para devolver el producto que necesitamos a la vista, el mismo que la id que se recibe por parametro. Hay que modificarlo para que use la DB en vez el JSON. 
-    /* edit: (req, res) => {
-        //Logica para devolver el producto encontrado a la vista y poder usar su ID para incluirla en el form
-        res.render('products/edit', {producto: db.Product.findByPk(req.params.id)})
-    }, */
-
     edit: async (req, res) => {
-        //Logica para devolver el producto encontrado a la vista y poder usar su ID para incluirla en el form
         let producto = await db.Product.findByPk(req.params.id)
         res.render('products/edit', { producto })
     },
@@ -73,37 +66,47 @@ const productsController = {
     createConfig: async (req, res) => {
         let errors = validationResult(req);
         if (errors.isEmpty()) {
-
-            db.Product.create({
-                name: req.body.nombre,
-                price: req.body.precio,
-                dues: req.body.cuotas,
-                discount: req.body.descuento,
-                stock: req.body.stock,
-                description: req.body.descripcion,
-                large_description: req.body.descripcionLarga,
-                upper: req.body.capellada,
-                cover: req.body.forro,
-                sole: req.body.suela,
-                origin: req.body.origen,
-                brand_id: req.body.marca,
-                image: req.file ? req.file.filename : ''
-            })
-            let ultimoProducto = await db.Product.findOne({
-                order: [['id', 'DESC']]
-            })
-            let coloresRecibidos = req.body.color
-
-            coloresRecibidos.forEach(color => {
-                db.Product_color.create({
-                    product_id: ultimoProducto.id + 1,
-                    color_id: color
-                })
-            })
-            res.redirect('/product/list')
-
+            try {
+                const productoCreado = await db.Product.create({
+                    name: req.body.nombre,
+                    price: req.body.precio,
+                    dues: req.body.cuotas,
+                    discount: req.body.descuento,
+                    stock: req.body.stock,
+                    description: req.body.descripcion,
+                    large_description: req.body.descripcionLarga,
+                    upper: req.body.capellada,
+                    cover: req.body.forro,
+                    sole: req.body.suela,
+                    origin: req.body.origen,
+                    brand_id: req.body.marca,
+                    image: req.file ? req.file.filename : ''
+                });
+                
+                let ultimoProducto = productoCreado;
+                let coloresRecibidos = req.body.color;
+                let tallesRecibidos = req.body.talle;
+    
+                coloresRecibidos.forEach(color => {
+                    db.Product_color.create({
+                        product_id: ultimoProducto.id,
+                        color_id: color
+                    });
+                });
+    
+                tallesRecibidos.forEach(talle => {
+                    db.Product_size.create({
+                        product_id: ultimoProducto.id,
+                        size_id: talle
+                    });
+                });
+    
+                res.redirect('/product/list');
+            } catch (error) {
+                res.send(error);
+            }
         } else {
-            res.render('products/create', { errors: errors.array(), old: req.body })
+            res.render('products/create', { errors: errors.array(), old: req.body });
         }
     },
 
