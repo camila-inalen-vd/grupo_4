57eb49b9d83c258772847630195a8dfea41033be
NoeCar <noeliacarabajal0@gmail.com>
2024-04-10 19:35:35 -0300

    arreglos

--- src/controllers/products-controller.js
+++ src/controllers/products-controller.js
@@ -31,7 +31,7 @@ const productsController = {
                         [Op.ne]: 0
                     }
                 },
-         
+         limit: 12
             });
 
             res.render("products/productDetail", { producto, interes});
